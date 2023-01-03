abd7018ad281918782c7fc4289ab5b77bf9d8c91
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-01-03 17:58:53 -0500

    folders

--- compile.sh
+++ compile.sh
@@ -1,6 +1,18 @@
+term=W23
 rm *-*.tex
 rm *-*.pdf
+mkdir -p forms
 python3 outline.py
+for file in `ls -1 ${term}*-*.tex`;
+do
+    pdflatex --interaction=batchmode $file 2>&1 > /dev/null;
+    pdflatex --interaction=batchmode $file 2>&1 > /dev/null;
+    pdflatex --interaction=batchmode $file 2>&1 > /dev/null;
+done
+mv ${term}*pdf forms
+mkdir -p sharepoint
+python3 outline.py sharepoint
+mkdir -p studyplan
 python3 diploma.py
 for file in `ls -1 *-*.tex`;
 do
@@ -9,6 +21,9 @@ do
     pdflatex --interaction=batchmode $file 2>&1 > /dev/null;
     pdflatex --interaction=batchmode $file 2>&1 > /dev/null;
 done
+mv ${term}*pdf  sharepoint
+mv studyplan*pdf studyplan
 rm *-*.toc
 rm *-*.out
-rm *-*.log	
+rm *-*.log
+rm *-*.aux
